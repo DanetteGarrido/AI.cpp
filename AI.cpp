c763c6780d2898e97b64f057685fb4876eebd91c
DanetteGarrido <113318258+DanetteGarrido@users.noreply.github.com>
2022-12-08 20:08:44 -0500

    Update AI.cpp

--- AI.cpp
+++ AI.cpp
@@ -4,8 +4,8 @@
  * AI.cpp
  * Project UID 28eb18c2c1ce490aada441e65559efdd
  *
- * <#Names#>
- * <#Uniqnames#>
+ * < Danette Garrido, Joseph Nunez, Nadia Jahan, Nakayla Butler >
+ * < danetteg, jsnunez, mosjah, nakaylab >
  *
  * Final Project - Elevators
  */
@@ -13,31 +13,29 @@
 #include "AI.h"
 #include <cassert>
 
-// This file is used only in the Reach, not the Core.
-// You do not need to make any changes to this file for the Core
 
-string getAIMoveString(const BuildingState& buildingState) {
-     int peopleInBuilding = 0;
+int peopleInBuilding(const BuildingState& buildingState) {
+    int numPeopleInBuilding = 0;
     for (int i = 0; i < 10; i++) {
+        numPeopleInBuilding += buildingState.floors[i].numPeople;
     }
-    if (peopleInBuilding == 0) {
-        return "";
+    if (numPeopleInBuilding == 0) {
+        return 0;
+    }
+    else {
+        return 1;
     }
 }
 
-string getAIPickupList(const Move& move, const BuildingState& buildingState, 
-                       const Floor& floorToPickup) {
-    return "";
-}
 
 //if the elevator has people on its floor and it isn't servicing
 int serviceReadyID(const BuildingState& buildingState){
     int openElevatorID = 0;
 
-for (int i = 0; i < NUM_ELEVATORS; i++) {
-    openElevatorID = i;
-    if (buildingState.floors[elevator.currentFloor].numPeople > 0 && !elevator.isServicing) {
-        if (i == 1) {
+     for (int i = 0; i < NUM_ELEVATORS; i++) {
+          openElevatorID = i;
+     if (buildingState.floors[elevator.currentFloor].numPeople > 0 && !elevator.isServicing) {
+          if (i == 1) {
             return "e1p";
         }
         if (i == 2){
@@ -49,14 +47,17 @@ for (int i = 0; i < NUM_ELEVATORS; i++) {
     }
     int openElevatorID;
 }
-     
 
-// have an hierarchy, first angriest, most requests, most people
-//figure out possible cases, can't have elvators in the same floor
-//- building is empty
-// - angry
-// - people per floor
-// 
-// have code that creates the string then worry abt changing the variables
-// break it into parts
-// you can have a bool
+string getAIMoveString(const BuildingState& buildingState) {
+    if (!peopleInBuilding) {
+        return "";
+    }
+    if (urgentFloor) {
+        return 
+    }
+}
+
+string getAIPickupList(const Move& move, const BuildingState& buildingState, 
+                       const Floor& floorToPickup) {
+    return "";
+}
